6eae70d5f6388480c349b167ce39f1f9731050fc
konsoletyper <konsoletyper@gmail.com>
2015-07-12 15:06:36 +0300

    Add TeaVM client

---
+++ ko-archetype/src/main/resources/archetype-resources/__teavmpath__/src/main/java/TeaVMMain.java
@@ -0,0 +1,10 @@
+package ${package};
+
+public class TeaVMMain {
+    private TeaVMMain() {
+    }
+
+    public static void main(String... args) throws Exception {
+        Main.onPageLoad();
+    }
+}
